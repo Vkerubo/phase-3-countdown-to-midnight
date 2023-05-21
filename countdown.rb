9173769bbc2bb45ba34e9f5712e7835a13d9a22b
Valentine <kerubovalentine1@gmail.com>
2023-05-21 07:57:39 +0300

    complete assignment

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
-#write your code here
 
-def countdown
+def countdown seconds
+  seconds = 10
+  while seconds > 0
+    puts "#{seconds} SECOND(S)!"
+    seconds -= 1
+  end
+   "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep number
+  while number > 0
+    puts "#{number} SECOND(S)!"
+    sleep (5)
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+countdown_with_sleep 10
+
